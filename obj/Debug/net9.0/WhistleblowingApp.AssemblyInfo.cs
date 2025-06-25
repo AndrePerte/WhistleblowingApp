a13a9dbfafaada596027bbb181ad70aa5f51b271
AndrePerte <andrea.pertegato@studio.unibo.it>
2025-06-25 11:43:46 +0200

    bin and obj changes

--- obj/Debug/net9.0/WhistleblowingApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/WhistleblowingApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WhistleblowingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09809e3ab84c54da7802ca194cc126963bafce1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WhistleblowingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WhistleblowingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
