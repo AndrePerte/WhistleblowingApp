e4a29d0438e3db3c344135b58b5117a8878768f4
AndrePerte <andrea.pertegato@studio.unibo.it>
2025-07-07 20:19:34 +0200

    Piccole correzioni

--- obj/Debug/net9.0/WhistleblowingApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/WhistleblowingApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WhistleblowingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eaeb574df50ddbd7a3260a573fd5d39d2eafcb16")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1387ec026b84484cb4c656be19c81f4ac0381e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("WhistleblowingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WhistleblowingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
